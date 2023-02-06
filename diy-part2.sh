f0c6bc5a04e83d0efcd1b6b677577c02d28f72ee
Andy-github999 <93528276+Andy-github999@users.noreply.github.com>
2023-02-06 22:02:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,8 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 
 # Modify default WiFi
 sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify cpu frequency
+sed -i 's/110,89/110,93/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i 's/u32 xtal_clk, cpu_clk, bus_clk;/u32 xtal_clk, cpu_clk, bus_clk, i;/g' target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
+sed -i "/+		pll = rt_memc_r32(MEMC_REG_CPU_PLL);/a+		pll &= ~(0x7ff);\n+		pll |= (0x362);\n+		rt_memc_w32(pll,MEMC_REG_CPU_PLL);\n+		for(i=0;i<1024;i++);" target/linux/ramips/patches-5.10/322-mt7621-fix-cpu-clk-add-clkdev.patch
