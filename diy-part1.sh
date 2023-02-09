5af278946a4ba0a03fd93c04ad041bfbb46cfa39
Andy-github999 <93528276+Andy-github999@users.noreply.github.com>
2023-02-09 22:18:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,8 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+git clone https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
+
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
